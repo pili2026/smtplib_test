f946f389599effd700202ab2071e4003f2e76ca6
pili2026 <b9813114@gmail.com>
2018-09-07 17:02:46 +0800

    This is mail test

--- smtplib_main.py
+++ smtplib_main.py
@@ -2,7 +2,7 @@ import email_smtplib_function
 
 smtplib_fun = email_smtplib_function.smtplib_test()
 smtplib_test = smtplib_fun.create_message_with_attchment("chiwei2026@gmail.com", 
-    "pili3722", "This is mail test for smtplib.", 
+    "xx", "This is mail test for smtplib.", 
     "I'm mail test,hello world.", 
     fileAtt=['/home/jeremy/PyProjects/smtplib_test/image.jpg'],
     To=["Chi-Wei.Shen@zyxel.com.tw"],
